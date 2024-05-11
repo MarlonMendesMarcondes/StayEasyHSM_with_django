e19bbf240cd5cba1004a7872ffd06701681de1e6
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2024-05-11 14:11:59 -0300

    Config settings.py

--- StayEasy/settings.py
+++ StayEasy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'StayEasy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,8 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [(os.path.join(STATIC_ROOT, 'static'))]
+
+MEDIA_URL = '/midia/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -139,5 +146,4 @@ JAZZMIN_SETTINGS = {
     'site_logo':"/images/logo.jpg",
     'copyright':"All rights Reserved 2023",
     'welcome_sign':"Welcome to StayEasy",
-   
 }
