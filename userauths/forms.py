130c5864250c6f875448ee2772c8cc8437b1892e
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2024-05-14 19:36:11 -0300

    Forms and homepage create

--- userauths/forms.py
+++ userauths/forms.py
@@ -3,6 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User, Profile
 
 class UserRegisterForm(UserCreationForm):
+    #first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Firstname', 'class': 'form-control w-50 m-2'}))
+    #last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Lastname', 'class': 'form-control w-50 m-2'}))
+    #username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Username', 'class': 'form-control w-50 m-2'}))
+    #email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Email', 'class': 'form-control w-50 m-2'}))
+    #phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Phone Number', 'class': 'form-control w-50 m-2'}))
+    #password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Your Password', 'class': 'form-control w-50 m-2'}))
+    #password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Your Password', 'class': 'form-control w-50 m-2'}))
     class Meta:
         model = User
-        fields = ['first_name','last_name','username','email','password1','password2']
+        fields = ['username','email', 'password1', 'password2']
--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from userauths.models import User, Profile
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
 
 def RegisterView(request):
-    form = UserRegisterForm()
+    if request.user.is_authenticated:
+        messages.warning(request, f" Hey {first_name} {last_name}, your already logged in.")
+        return redirect("hotel:index")
+    form = UserRegisterForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        phone = form.cleaned_data.get('phone')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password1')
+        
+        user = authenticate(email=email,password = password)
+        login(request, user)
+        
+        messages.success(request, f" Hey {first_name} {last_name}, your account has been created sucessfully.")
+        
+        profile = Profile.objects.get(user=request.user)
+        profile.first_name = first_name
+        profile.last_name = last_name
+        profile.phone = phone
+        profile.email = email
+        profile.save()
+        
+        return redirect("")
     context = {
         "form":form
     }
