797b6b9ada3eefa9b2d6e15b881955f90c19cb77
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2024-05-16 22:33:36 -0300

    register form is ready but return IntegrityErro

--- userauths/forms.py
+++ userauths/forms.py
@@ -3,13 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User, Profile
 
 class UserRegisterForm(UserCreationForm):
-    #first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Firstname', 'class': 'form-control w-50 m-2'}))
-    #last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Lastname', 'class': 'form-control w-50 m-2'}))
-    #username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Username', 'class': 'form-control w-50 m-2'}))
-    #email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Email', 'class': 'form-control w-50 m-2'}))
-    #phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Phone Number', 'class': 'form-control w-50 m-2'}))
-    #password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Your Password', 'class': 'form-control w-50 m-2'}))
-    #password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Your Password', 'class': 'form-control w-50 m-2'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Firstname', 'class': ''}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Lastname', 'class': ''}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Username', 'class': ''}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Email', 'class': ''}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Your Phone Number', 'class': ''}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Your Password', 'class': ''}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Your Password', 'class': ''}))
     class Meta:
         model = User
-        fields = ['username','email', 'password1', 'password2']
+        fields = ['first_name','last_name', 'phone' ,'username','email', 'password1', 'password2']
